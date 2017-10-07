5f548c4b89067313ebbfe73fa8d89df80ef08ba0
agustin <adammiano@itba.edu.ar>
2017-10-07 17:41:08 -0300

    Algo turbio: filtro de los comandos

--- src/Master.java
+++ src/Master.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Master {
     private BlockChain blockChain;
     private AVLTree avlTree;
@@ -8,6 +12,53 @@ public class Master {
 
         this.blockChain = new BlockChain(zeros);
         this.avlTree = new AVLTree();
+    }
+
+    public void run(){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int error;
+        String input = new String();
+        error = 0;
+        try {
+            input = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        error = command(input);
+        //printError(error);
+    }
+
+    private static String fliter[]={"add [0-9]+","remove [0-9]+", "lookup [0-9]+", "validate", "modify "};
+
+    //valida y ejecuta
+    public int command(String s){   //se podra hacer mejor?
+        int aux = 0;
+        if (s.matches(fliter[0])){
+            add(getNumber(s.toCharArray(),4, s.length()));
+        }
+        if (s.matches(fliter[1])){
+            remove(getNumber(s.toCharArray(),7, s.length()));
+        }
+        if (s.matches(fliter[2])){
+            lookup(getNumber(s.toCharArray(),7, s.length()));
+        }
+        if (s.matches(fliter[3])){
+            validate();
+        }
+        if (s.matches(fliter[4])){
+            //validar el path del archivo
+        }
+
+        return aux;
+    }
+
+    public static int getNumber(char c[],int first, int last){
+        int aux=c[first]-'0';
+        while (first<last){
+            first++;
+            aux=aux*10+c[first]-'0';
+        }
+        return aux;
     }
 
     public void add(int number)
