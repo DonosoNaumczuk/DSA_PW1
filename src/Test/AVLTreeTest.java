9de5146b276a590b83979d87174ee075aab58e1b
DonosoNaumczuk <adonoso@itba.edu.ar>
2017-10-11 13:08:22 -0300

    Update AVLTree JUnit Test

--- src/Test/AVLTreeTest.java
+++ src/Test/AVLTreeTest.java
@@ -7,118 +7,103 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class AVLTreeTest {
-    private AVLTree tree;
-//    @Before
-//    public void Before() {
-//        tree = new AVLTree();
-//    }
 
-    @Test
-    public void addingNodeTest(){
-        AVLTree t = new AVLTree();
-        t.add(10);
-        assertEquals("Deberia tener un nodo.",1,t.getNodeQty());
-    }
-
-    @Test
-    public void addingNodeCorrectlyTest(){
-        AVLTree t = new AVLTree();
-        boolean added = t.add(10);
-        assertEquals("Deberia tener un nodo.",1,t.getNodeQty());
-        assertEquals("Deberia haber dado true.",true,added);
-    }
-
-    @Test
-    public void addingAnExistingNodeTest(){
-        AVLTree t = new AVLTree();
-        t.add(10);
-        t.add(5);
-        boolean added = t.add(5);
-        assertEquals("Deberia tener dos nodos.",2,t.getNodeQty());
-        assertEquals("Deberia haber dado false.",false,added);
-    }
-
-
+    private AVLTree beforeTree;
 
     @Before
-    public void before(){
-        tree = new AVLTree();
-        tree.add(5);
-        tree.add(9);
-        tree.add(100);
-        tree.add(14);
-        tree.add(-10);
+    public void before() {
+        beforeTree = new AVLTree();
+        beforeTree.add(5);
+        beforeTree.add(9);
+        beforeTree.add(100);
+        beforeTree.add(14);
+        beforeTree.add(-10);
+        beforeTree.add(10);
+    }
+
+    @Test
+    public void addingExistingNodeTest() {
+        AVLTree tree = new AVLTree();
         tree.add(10);
+        tree.add(5);
+        boolean returnedValue = tree.add(5);
+        assertEquals("Must have two nodes.", 2, tree.getNodeQty());
+        assertEquals("Must be false.", false, returnedValue);
     }
 
     @Test
-    public void addingBalancingTreeTest(){
+    public void addingNoneExistingNodeTest() {
+        AVLTree tree = new AVLTree();
+        tree.add(10);
+        tree.add(2);
+        boolean returnedValue = tree.add(5);
+        assertEquals("Must have three nodes.", 3, tree.getNodeQty());
+        assertEquals("Must be true.", true, returnedValue);
+    }
+
+    @Test
+    public void balanceTreeTest() {
+        AVLTree tree = beforeTree;
         tree.add(1000);
-        assertEquals("Deberia estar balanceado.",true,tree.isBalanced());
+        assertEquals("Must be balanced.", true, tree.isBalanced());
     }
 
     @Test
-    public void succesfulEqualsTreeTest(){
-        AVLTree t = tree;
-        assertEquals("Deberian ser iguales los arboles.",true,tree.equals(t));
-        AVLTree t2 = new AVLTree();
-        t2.add(9);
-        t2.add(14);
-        t2.add(-10);
-        t2.add(5);
-        t2.add(10);
-        t2.add(100);
-        assertEquals("Deberian ser iguales los arboles.",true,tree.equals(t));
+    public void comparingEqualTreesTest() {
+        AVLTree tree1 = beforeTree;
+        AVLTree tree2 = new AVLTree();
+        tree2.add(9);
+        tree2.add(14);
+        tree2.add(-10);
+        tree2.add(5);
+        tree2.add(10);
+        tree2.add(100);
+        assertEquals("Equals method must return true.", true, tree1.equals(tree2));
     }
 
     @Test
-    public void unsuccesfulEqualsTreeTest(){
-        AVLTree t = new AVLTree();
-        t.add(9);
-        t.add(14);
-        t.add(-10);
-        t.add(5);
-        t.add(10);
-        t.add(100);
-        t.add(1);
-        assertEquals("Deberian ser distintos los arboles.",false,tree.equals(t));
-        tree.add(1);
-        tree.add(104); //with an extra right child
-        assertEquals("Deberian ser distintos los arboles.",false,tree.equals(t));
-        t.add(104);
-        tree.add(-14);//with an extra left child
-        assertEquals("Deberian ser distintos los arboles.",false,tree.equals(t));
-        AVLTree t2 = new AVLTree();
-        t2.add(10);
-        t2.add(16);
-        assertEquals("Deberian ser distintos los arboles.",false,tree.equals(t2));
+    public void comparingNotEqualTreesTest() {
+        AVLTree tree1 = new AVLTree();
+        tree1.add(9);
+        tree1.add(14);
+        tree1.add(-10);
+        tree1.add(5);
+        tree1.add(10);
+        tree1.add(100);
+        tree1.add(1);
+        assertEquals("Equals method must return false.", false, beforeTree.equals(tree1));
+        beforeTree.add(1);
+        beforeTree.add(104);  /* With an extra right child */
+        assertEquals("Equals method must return false.", false, beforeTree.equals(tree1));
+        tree1.add(104);
+        beforeTree.add(-14); /* With an extra left child */
+        assertEquals("Equals method must return false.", false, beforeTree.equals(tree1));
+        AVLTree tree2 = new AVLTree();
+        tree2.add(10);
+        tree2.add(16);
+        assertEquals("Equals method must return false.", false, beforeTree.equals(tree2));
     }
 
     @Test
-    public void removingNodeTest(){
-        int qty = tree.getNodeQty();
+    public void removingExistingNodeTest() {
+        AVLTree tree = beforeTree;
+        int beforeQty = tree.getNodeQty();
         tree.remove(10);
-        assertEquals("Deberia tener un nodo menos.",qty-1,tree.getNodeQty()); //see if only one node removed
-        boolean value =tree.add(10); //see if 10 was removed
-        assertEquals("10 no deberia estar en el arbol.",true,value);
+        int currentQty = tree.getNodeQty();
+        assertEquals("Deberia tener un nodo menos.", beforeQty - 1, currentQty);
+        boolean wasAdded = tree.add(10);
+        assertEquals("Variable wasAdded must be true.", true, wasAdded);
     }
+
 
     @Test
-    public void removingNodeCorrectlyTest(){
-        int qty = tree.getNodeQty();
-        boolean removed = tree.remove(10);
-        assertEquals("Deberia tener un nodo menos.",qty-1,tree.getNodeQty());
-        assertEquals("Deberia haber dado true.",true,removed);
+    public void removingNoneExistingNodeTest() {
+        AVLTree tree = beforeTree;
+        int beforeQty = tree.getNodeQty();
+        boolean wasRemoved = tree.remove(-1);
+        int currentQty = tree.getNodeQty();
+        assertEquals("Node quantity must be equal.", currentQty, beforeQty);
+        assertEquals("Variable wasRemoved must be false.", false, wasRemoved);
     }
-
-    @Test
-    public void removingNoneExistingNodeTest(){
-        int qty = tree.getNodeQty();
-        boolean removed = tree.remove(-1);
-        assertEquals("Deberia dar la misma cantidad de nodos.",qty,tree.getNodeQty());
-        assertEquals("Deberia haber dado false.",false,removed);
-    }
-
-
 
 }
