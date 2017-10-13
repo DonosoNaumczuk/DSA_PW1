7e1c3e543062cef38718d42589272fb6ce2f3f28
DonosoNaumczuk <adonoso@itba.edu.ar>
2017-10-13 02:18:51 -0300

    BlockChainTest added

--- src/Test/BlockChainTest.java
+++ src/Test/BlockChainTest.java
@@ -1,4 +1,22 @@
+import BackEnd.BlockChain;
+import BackEnd.SHA256;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class BlockChainTest {
 
+    @Before
+    public void before() {
+        BlockChain blockChain = new BlockChain(3, new SHA256());
+    }
+
+    @Test
+    public void addOperationTest() {
+        BlockChain blockChain = new BlockChain(3, new SHA256());
+        blockChain.add(6);
+        blockChain.add(9);
+        long blockQty = blockChain.blockQty();
+        assertEquals("Must have 2 blocks.", 2, blockQty);
+    }
 }
